4deefbede510d95473db2fefc87cee95ed029e78
leobath <leoneedass@gmail.com>
2020-10-20 22:32:06 +0300

    fifth

---
+++ clang/second.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+void count(int &a);
+
+int main(int argc, char* argv[]) {
+    int s = 0;
+    count(s);
+    std::cout << s << std::endl;
+}
+
+void count(int &a) {
+    a++;
+}

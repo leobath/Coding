5943d1e0c03f613c4f5da942da0f87a66e5ba877
leobath <leoneedass@gmail.com>
2020-12-06 22:14:50 +0200

    less

--- python/new.py
+++ python/new.py
@@ -2,5 +2,13 @@ healthy = ["kale chips", "broccoli"]
 backpack = ["pizza", "apple crisp", "kale chips"]
 
 print(backpack)
-backpack[:] = [item for item in backpack if item in healthy]
+#backpack[:] = [item.upper() for item in backpack if item in healthy]
+
+healthy_backpack = [item.upper() for item in backpack if item in healthy]
+
+# for item in backpack:
+#     if item in healthy:
+#         healthy_backpack.append(item.upper())
+
 print(backpack)
+print(healthy_backpack)
---
+++ python/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+
+# for item in backpack:
+#     if item in healthy:
+#         healthy_backpack.append(item.upper())

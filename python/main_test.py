f3f41a1b8f017ac07baf66b1e5cf76883e5ef762
leobath <leoneedass@gmail.com>
2020-12-19 19:17:48 +0200

    test3

--- python/main_test.py
+++ python/main_test.py
@@ -2,7 +2,7 @@ def main():
     print("insert your name:")
     name = input()
     print("your name is", name)
-    print('bye bye')
+    print('bye bye', __name__)
 
 if __name__ == "__main__":
     main()

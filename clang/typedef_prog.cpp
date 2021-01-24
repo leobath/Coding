928cb69951cc5891ca9585e856c8a4947831e8a5
leobath <leoneedass@gmail.com>
2021-01-24 14:53:42 +0200

    typedef custom types

---
+++ clang/typedef_prog.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+typedef unsigned long myagetype;
+
+size_t usin;
+
+int main(int argc, char const *argv[])
+{
+    myagetype age = 44.3;
+    std::cout << age << std::endl;
+    usin  = 33.4;
+    std::cout << usin << std::endl;
+    return 0;
+}
